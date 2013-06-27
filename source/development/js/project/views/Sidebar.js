6e66896abc43d867025038ae72c58a1d2bd4525e
yixiongz <yixiong.zhou@b-reel.com>
2013-06-27 18:45:14 -0400

    added sidebar content template

--- source/development/js/project/views/Sidebar.js
+++ source/development/js/project/views/Sidebar.js
@@ -3,6 +3,7 @@ goog.provide('hlc.views.Sidebar');
 goog.require('goog.events.EventTarget');
 goog.require('goog.events');
 goog.require('goog.dom');
+goog.require('goog.net.XhrIo');
 
 /**
  * @constructor
@@ -15,6 +16,12 @@ hlc.views.Sidebar = function(){
 
   this._size = goog.style.getSize(this.domElement);
   this._mainScrollerDomElement = goog.dom.getElement('main-scroller');
+
+  this._contentDomElements = {
+  	'albumId': {'songId': ''}
+  };
+
+  this._request = new goog.net.XhrIo();
 };
 goog.inherits(hlc.views.Sidebar, goog.events.EventTarget);
 
@@ -45,6 +52,47 @@ hlc.views.Sidebar.prototype.slideOut = function(){
 	this.isSlidedIn = false;
 
 	goog.style.setStyle(this._mainScrollerDomElement, 'width', '100%');
+};
+
+
+hlc.views.Sidebar.prototype.loadContent = function(albumId, songId){
+	// cancel current loading
+	goog.events.removeAll(this._request, "complete");
+	this._request.abort();
+
+	// skip loading if content is already loaded
+	if(this._contentDomElements[albumId]) {
+		if(this._contentDomElements[albumId][songId]) {
+			this.onLoaded(albumId, songId);
+			return;
+		}
+	}
+
+	// load new content
+	var url = hlc.Url.ORIGIN + 'detail?album=' + albumId + '&song=' + songId;
+
+	goog.events.listenOnce(this._request, "complete", function(e) {
+
+		if(e.target.isSuccess()) {
+			var responseText = e.target.getResponseText();
+			//console.log(responseText);
+
+			if(!this._contentDomElements[albumId]) this._contentDomElements[albumId] = [];
+			if(!this._contentDomElements[albumId][songId]) this._contentDomElements[albumId][songId] = responseText;
+
+			this.onLoaded(albumId, songId);
+		}else {
+			console.log(e.target.getLastError(), this);
+		}
+
+	}, false, this);
+
+	this._request.send(url);
+};
+
+
+hlc.views.Sidebar.prototype.onLoaded = function(albumId, songId) {
+	this.domElement.innerHTML = this._contentDomElements[albumId][songId];
 };
 
 
