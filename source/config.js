8fed03020806bef728b8fce188b4fe29a21b5bc8
joe <yixiong.zhou@b-reel.com>
2013-06-30 23:37:28 -0400

    COMPILED MILESTONE

--- source/config.js
+++ source/config.js
@@ -3,11 +3,13 @@
   // "This config file is used to test org.plovr.Main."
   "id": "hlc-portfolio",
   "paths": ["development"],
-  "inputs": "development/js/main.js",
+  "inputs": "development/js/project/main.js",
   "mode": "ADVANCED",
   "level": "QUIET",
   "externs": [
+    "development/js/externs.js",
   	"../public/assets/static/js/third-party/greensock/TweenMax.min.js",
-  	"../public/assets/static/js/third-party/greensock/plugins/ScrollToPlugin.min.js"
+  	"../public/assets/static/js/third-party/greensock/plugins/ScrollToPlugin.min.js",
+    "../public/assets/static/js/third-party/paper-full.min.js"
 	]
 }
