f81de3a71e407c9610e6e23ec921af2e50377dba
yixiongz <yixiong.zhou@b-reel.com>
2013-07-26 16:36:12 -0400

    added a preload screen;

---
+++ source/development/js/project/events/events.js
@@ -0,0 +1,8 @@
+goog.provide('hlc.events');
+
+hlc.events.EventType = {
+	ANIMATE_IN_START: 'animate_in_start',
+	ANIMATE_OUT_START: 'animate_out_start',
+	ANIMATE_IN_COMPLETE: 'animate_in_complete',
+	ANIMATE_OUT_COMPLETE: 'animate_out_complete'
+};
