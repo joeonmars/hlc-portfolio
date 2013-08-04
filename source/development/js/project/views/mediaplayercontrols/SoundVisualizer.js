6bba27cf2b00e440c4d49bfdd32427842436cd27
joe <yixiong.zhou@b-reel.com>
2013-08-04 00:58:00 -0400

    added purple gradient to sound visualizer;

--- source/development/js/project/views/mediaplayercontrols/SoundVisualizer.js
+++ source/development/js/project/views/mediaplayercontrols/SoundVisualizer.js
@@ -2,6 +2,7 @@ goog.provide('hlc.views.mediaplayercontrols.SoundVisualizer');
 
 goog.require('goog.events.EventTarget');
 goog.require('goog.dom');
+goog.require('goog.color');
 
 /**
  * @constructor
@@ -42,15 +43,31 @@ hlc.views.mediaplayercontrols.SoundVisualizer.prototype.draw = function(audioDat
   var shapeAmount = Math.round( this.canvas.width / 2 );
   var shapeWidth = this.canvas.width / shapeAmount;
 
+  var purpleInRgb = goog.color.hexToRgb('#535362');
+  var gradients = goog.array.repeat(purpleInRgb, shapeAmount);
+  var halfAmount = shapeAmount / 2;
+  gradients = goog.array.map(gradients, function(color, index) {
+    var lighten = (index <= halfAmount) ? (index / halfAmount) : (1 - (index - halfAmount) / halfAmount);
+    return goog.color.lighten(color, lighten);
+  });
+
   for( var i = 0; i < shapeAmount; i ++ ) {
     var shapeHeight = audioData[Math.round(i * (audioData.length / shapeAmount))] / 4;
     var size = new paper.Size(shapeWidth, shapeHeight);
     var point = new paper.Point(i * shapeWidth, (this.canvas.height - shapeHeight) / 2);
     var shape = new paper.Path.Rectangle(point, size);
 
+    var gradientColor = gradients[i];
+    var r = gradientColor[0] / 225;
+    var g = gradientColor[1] / 225;
+    var b = gradientColor[2] / 225;
+    var topColor = new paper.Color(r, g, b, 0);
+    var midColor = new paper.Color(r, g, b, .2);
+    var bottomColor = new paper.Color(r, g, b, 0);
+
     shape.fillColor = {
       'gradient': {
-        'stops': [new paper.Color(1, 1, 1, 0), new paper.Color(1, 1, 1, .2), new paper.Color(1, 1, 1, 0)]
+        'stops': [topColor, midColor, bottomColor]
       },
       'origin': shape.bounds.topLeft,
       'destination': shape.bounds.bottomLeft
