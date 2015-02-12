3b9400895a6d07efd09ce02d92480f42e831b1e6
joeonmars <joe@joeonmars.com>
2015-02-11 22:40:52 -0500

    update;

--- source/config.js
+++ source/config.js
@@ -12,7 +12,6 @@
   	"../public/assets/static/js/third-party/greensock/plugins/ScrollToPlugin.min.js",
     "../public/assets/static/js/third-party/greensock/plugins/ThrowPropsPlugin.min.js",
     "../public/assets/static/js/third-party/greensock/utils/Draggable.min.js",
-    "../public/assets/static/js/third-party/paper-full.min.js",
-    "../public/assets/static/js/third-party/modernizr-latest.js"
+    "../public/assets/static/js/third-party/paper-full.min.js"
 	]
 }
