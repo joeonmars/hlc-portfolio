95b8b2e90fb905bf9f24fd784907c0aea1e1d811
joeonmars <joe@joeonmars.com>
2015-02-12 09:53:50 -0500

    added pixi;

--- source/config.js
+++ source/config.js
@@ -12,6 +12,7 @@
   	"../public/assets/static/js/third-party/greensock/plugins/ScrollToPlugin.min.js",
     "../public/assets/static/js/third-party/greensock/plugins/ThrowPropsPlugin.min.js",
     "../public/assets/static/js/third-party/greensock/utils/Draggable.min.js",
-    "../public/assets/static/js/third-party/paper-full.min.js"
+    "../public/assets/static/js/third-party/paper-full.min.js",
+    "../public/assets/static/js/third-party/pixi.js"
 	]
 }
