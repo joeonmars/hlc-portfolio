f1a998b56aa32ae97dc341a0f712c4a2a840a278
joeonmars <joe@joeonmars.com>
2016-02-27 23:00:01 -0500

    fixed the css background extraction error since chrome 47;

--- source/development/js/project/utils.js
+++ source/development/js/project/utils.js
@@ -7,7 +7,7 @@ goog.require( 'goog.string' );
 goog.require( 'goog.Uri' );
 
 
-hlc.utils.popUpWindow = function(url, width, height, options) {
+hlc.utils.popUpWindow = function( url, width, height, options ) {
 	var viewportSize = goog.dom.getViewportSize();
 
 	var options = options || {
@@ -18,75 +18,96 @@ hlc.utils.popUpWindow = function(url, width, height, options) {
 		'resizable': false
 	};
 
-	goog.window.open(url, {
+	goog.window.open( url, {
 		'width': width,
 		'height': height,
-		'left': (window.screenLeft || window.screenX) + (viewportSize.width - width)/2,
-		'top': (window.screenTop || window.screenY) + (viewportSize.height - height)/2,
-		'toolbar': options['toolbar'],
-		'scrollbars': options['scrollbars'],
-		'statusbar': options['statusbar'],
-		'menubar': options['menubar'],
-		'resizable': options['resizable']
-	});
+		'left': ( window.screenLeft || window.screenX ) + ( viewportSize.width - width ) / 2,
+		'top': ( window.screenTop || window.screenY ) + ( viewportSize.height - height ) / 2,
+		'toolbar': options[ 'toolbar' ],
+		'scrollbars': options[ 'scrollbars' ],
+		'statusbar': options[ 'statusbar' ],
+		'menubar': options[ 'menubar' ],
+		'resizable': options[ 'resizable' ]
+	} );
 };
 
 
 hlc.utils.runSocialButtonScripts = function() {
-		/* facebook */
-		var fbRootDom = goog.dom.createDom('div', {'id': 'fb-root'});
-		goog.dom.appendChild(document.body, fbRootDom);
+	/* facebook */
+	var fbRootDom = goog.dom.createDom( 'div', {
+		'id': 'fb-root'
+	} );
+	goog.dom.appendChild( document.body, fbRootDom );
 
-		(function(d, s, id) {
-		  var js, fjs = d.getElementsByTagName(s)[0];
-		  if (d.getElementById(id)) return;
-		  js = d.createElement(s); js.id = id;
-		  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-		  fjs.parentNode.insertBefore(js, fjs);
-		}(document, 'script', 'facebook-jssdk'));
+	( function( d, s, id ) {
+		var js, fjs = d.getElementsByTagName( s )[ 0 ];
+		if ( d.getElementById( id ) ) return;
+		js = d.createElement( s );
+		js.id = id;
+		js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+		fjs.parentNode.insertBefore( js, fjs );
+	}( document, 'script', 'facebook-jssdk' ) );
 
-		/* twitter */
-		!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+	/* twitter */
+	! function( d, s, id ) {
+		var js, fjs = d.getElementsByTagName( s )[ 0 ],
+			p = /^http:/.test( d.location ) ? 'http' : 'https';
+		if ( !d.getElementById( id ) ) {
+			js = d.createElement( s );
+			js.id = id;
+			js.src = p + '://platform.twitter.com/widgets.js';
+			fjs.parentNode.insertBefore( js, fjs );
+		}
+	}( document, 'script', 'twitter-wjs' );
 
-		/* google plus */
-	  (function() {
-	    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-	    po.src = 'https://apis.google.com/js/plusone.js';
-	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-	  })();
+	/* google plus */
+	( function() {
+		var po = document.createElement( 'script' );
+		po.type = 'text/javascript';
+		po.async = true;
+		po.src = 'https://apis.google.com/js/plusone.js';
+		var s = document.getElementsByTagName( 'script' )[ 0 ];
+		s.parentNode.insertBefore( po, s );
+	} )();
 
-		/* getglue */
-		var scriptTag = goog.dom.createDom('script', {'src': "http://widgets.getglue.com/checkin.js", 'type': "text/javascript"});
-		goog.dom.appendChild(document.body, scriptTag);
+	/* getglue */
+	var scriptTag = goog.dom.createDom( 'script', {
+		'src': "http://widgets.getglue.com/checkin.js",
+		'type': "text/javascript"
+	} );
+	goog.dom.appendChild( document.body, scriptTag );
 };
 
 
-hlc.utils.getTouchCoordinate = function(e) {
-	var touchX = e.touches ? e.touches[0].pageX : e.clientX;
-	var touchY = e.touches ? e.touches[0].pageY : e.clientY;
-	var coord = {x:touchX, y:touchY};
+hlc.utils.getTouchCoordinate = function( e ) {
+	var touchX = e.touches ? e.touches[ 0 ].pageX : e.clientX;
+	var touchY = e.touches ? e.touches[ 0 ].pageY : e.clientY;
+	var coord = {
+		x: touchX,
+		y: touchY
+	};
 	return coord;
 }
 
 
-hlc.utils.grabCursor = function(domElement) {
+hlc.utils.grabCursor = function( domElement ) {
 	var handler = new goog.events.EventHandler();
 
 	var add = function() {
-		goog.dom.classes.add(domElement, 'grab');
+		goog.dom.classes.add( domElement, 'grab' );
 
-		handler.listen(domElement, 'mousedown', function(e) {
+		handler.listen( domElement, 'mousedown', function( e ) {
 			e.preventDefault();
-			goog.dom.classes.add(domElement, 'grabbing');
-		});
+			goog.dom.classes.add( domElement, 'grabbing' );
+		} );
 
-		handler.listen(domElement, 'mouseup', function(e) {
-			goog.dom.classes.remove(domElement, 'grabbing');
-		});
+		handler.listen( domElement, 'mouseup', function( e ) {
+			goog.dom.classes.remove( domElement, 'grabbing' );
+		} );
 	};
 
 	var remove = function() {
-		goog.dom.classes.remove(domElement, 'grab');
+		goog.dom.classes.remove( domElement, 'grab' );
 		handler.removeAll();
 	};
 
@@ -105,27 +126,27 @@ hlc.utils.grabCursor = function(domElement) {
 }
 
 
-hlc.utils.getBackgroundImageUrl = function(domElement) {
-	return domElement.style.backgroundImage.slice(4, -1);
+hlc.utils.getBackgroundImageUrl = function( domElement ) {
+	return domElement.style.backgroundImage.match( /url\(["|']?([^"']*)["|']?\)/ )[ 1 ];
 };
 
 
 hlc.utils.isTablet = function() {
-	return (goog.userAgent.IPAD || goog.userAgent.ANDROID || goog.userAgent.WINDOWS);
+	return ( goog.userAgent.IPAD || goog.userAgent.ANDROID || goog.userAgent.WINDOWS );
 };
 
 
 hlc.utils.isRetina = function() {
-	return !(window.devicePixelRatio === 1);
+	return !( window.devicePixelRatio === 1 );
 };
 
 
-hlc.utils.getQuery = function(key) {
+hlc.utils.getQuery = function( key ) {
 
-  var uri = new goog.Uri( window.location.href );
-  var queryData = uri.getQueryData();
+	var uri = new goog.Uri( window.location.href );
+	var queryData = uri.getQueryData();
 
-  return queryData.get(key);
+	return queryData.get( key );
 };
 
 
@@ -139,7 +160,7 @@ hlc.utils.coverFit = function( sprite, viewWidth, viewHeight ) {
 
 	var scaledWidth, scaledHeight;
 
-	if (viewRatio > textureRatio) {
+	if ( viewRatio > textureRatio ) {
 
 		scaledWidth = viewWidth;
 		scaledHeight = scaledWidth / textureRatio;
@@ -153,6 +174,6 @@ hlc.utils.coverFit = function( sprite, viewWidth, viewHeight ) {
 	sprite.width = scaledWidth;
 	sprite.height = scaledHeight;
 
-	sprite.x = (viewWidth - scaledWidth) / 2;
-	sprite.y = (viewHeight - scaledHeight) / 2;
+	sprite.x = ( viewWidth - scaledWidth ) / 2;
+	sprite.y = ( viewHeight - scaledHeight ) / 2;
 };
--- source/development/js/project/views/Preloader.js
+++ source/development/js/project/views/Preloader.js
@@ -12,39 +12,39 @@ hlc.views.Preloader = function() {
 	var bulkAssets = {
 		'sitemap': hlc.Url.ORIGIN + 'sitemap.html'
 	};
-	
+
 	var imageAssets = {};
 
-	goog.object.forEach(hlc.main.data.preloadAssets, function(url, id) {
-		goog.object.add(imageAssets, id, url);
-	});
+	goog.object.forEach( hlc.main.data.preloadAssets, function( url, id ) {
+		goog.object.add( imageAssets, id, url );
+	} );
 
-	goog.base(this, bulkAssets, imageAssets, 4000);
+	goog.base( this, bulkAssets, imageAssets, 4000 );
 
 	this.assets = {};
-	this._domElement = goog.dom.getElement('preloader');
+	this._domElement = goog.dom.getElement( 'preloader' );
 
-	this._innerEl = goog.dom.getElementByClass('inner', this._domElement);
-	this._curtainEl = goog.dom.getElementByClass('curtain', this._domElement);
+	this._innerEl = goog.dom.getElementByClass( 'inner', this._domElement );
+	this._curtainEl = goog.dom.getElementByClass( 'curtain', this._domElement );
 
-	this._contentEl = goog.dom.getElementByClass('content', this._domElement);
+	this._contentEl = goog.dom.getElementByClass( 'content', this._domElement );
 
-	this._portraitEl = goog.dom.getElementByClass('portrait', this._domElement);
+	this._portraitEl = goog.dom.getElementByClass( 'portrait', this._domElement );
 
-	this._hintEl = goog.dom.getElementByClass('hint', this._domElement);
+	this._hintEl = goog.dom.getElementByClass( 'hint', this._domElement );
 
-	this._slashEl = goog.dom.getElementByClass('slash', this._domElement);
+	this._slashEl = goog.dom.getElementByClass( 'slash', this._domElement );
 
-	this._numeratorEl = goog.dom.getElementByClass('numerator', this._domElement);
-	this._denominatorEl = goog.dom.getElementByClass('denominator', this._domElement);
+	this._numeratorEl = goog.dom.getElementByClass( 'numerator', this._domElement );
+	this._denominatorEl = goog.dom.getElementByClass( 'denominator', this._domElement );
 
-	this._numeratorOnesEl = goog.dom.query('.ones', this._numeratorEl)[0];
-	this._numeratorTensEl = goog.dom.query('.tens', this._numeratorEl)[0];
-	this._numeratorHundredsEl = goog.dom.query('.hundreds', this._numeratorEl)[0];
+	this._numeratorOnesEl = goog.dom.query( '.ones', this._numeratorEl )[ 0 ];
+	this._numeratorTensEl = goog.dom.query( '.tens', this._numeratorEl )[ 0 ];
+	this._numeratorHundredsEl = goog.dom.query( '.hundreds', this._numeratorEl )[ 0 ];
 
-	this._numeratorOnesDigits = goog.dom.query('span', this._numeratorOnesEl);
-	this._numeratorTensDigits = goog.dom.query('span', this._numeratorTensEl);
-	this._numeratorHundredsDigits = goog.dom.query('span', this._numeratorHundredsEl);
+	this._numeratorOnesDigits = goog.dom.query( 'span', this._numeratorOnesEl );
+	this._numeratorTensDigits = goog.dom.query( 'span', this._numeratorTensEl );
+	this._numeratorHundredsDigits = goog.dom.query( 'span', this._numeratorHundredsEl );
 
 	//
 	this._progressUpdateThrottle = new goog.async.Throttle( this.renderProgress, 350, this );
@@ -53,103 +53,105 @@ hlc.views.Preloader = function() {
 	this._animationProgress = 0;
 
 	// animation
-	var fadeInTweener = TweenMax.to(this._innerEl, .45, {
+	var fadeInTweener = TweenMax.to( this._innerEl, .45, {
 		opacity: 1,
 		display: 'block',
 		ease: Cubic.easeInOut
-	});
+	} );
 
-	var portraitInTweener = TweenMax.fromTo(this._portraitEl, .8, {
+	var portraitInTweener = TweenMax.fromTo( this._portraitEl, .8, {
 		opacity: 0,
 		scale: .8
-	},{
+	}, {
 		opacity: 1,
 		scale: 1,
 		ease: Expo.easeOut
-	});
+	} );
 
-	this._animateInTweener.add(fadeInTweener);
-	this._animateInTweener.add(portraitInTweener, '+=0', 'start', .25);
+	this._animateInTweener.add( fadeInTweener );
+	this._animateInTweener.add( portraitInTweener, '+=0', 'start', .25 );
 
-	var contentOutTweener = TweenMax.to(this._contentEl, .8, {
+	var contentOutTweener = TweenMax.to( this._contentEl, .8, {
 		opacity: 0,
 		y: -80,
 		ease: Cubic.easeInOut
-	});
+	} );
 
-	var portraitOutTweener = TweenMax.to(this._portraitEl, .8, {
+	var portraitOutTweener = TweenMax.to( this._portraitEl, .8, {
 		delay: .25,
 		opacity: 0,
 		y: -40,
 		ease: Cubic.easeInOut
-	});
+	} );
 
-	var curtainTweener = TweenMax.fromTo(this._curtainEl, 1, {
+	var curtainTweener = TweenMax.fromTo( this._curtainEl, 1, {
 		opacity: 0
-	},{
+	}, {
 		delay: .5,
 		opacity: 1,
 		display: 'block',
 		ease: Cubic.easeInOut
-	});
+	} );
 
-	this._animateOutTweener.add([contentOutTweener, portraitOutTweener, curtainTweener], '+=0', 'start', 0);
+	this._animateOutTweener.add( [ contentOutTweener, portraitOutTweener, curtainTweener ], '+=0', 'start', 0 );
 };
-goog.inherits(hlc.views.Preloader, hlc.views.Loader);
+goog.inherits( hlc.views.Preloader, hlc.views.Loader );
 
 
-hlc.views.Preloader.prototype.init = function(){
+hlc.views.Preloader.prototype.init = function() {
 
 	// set initial style
 	var i, l = 10;
 
-	for(i = 0; i < l; i++) {
-		goog.dom.classes.enable( this._numeratorOnesDigits[i], 'hidden', true );
-		goog.dom.classes.enable( this._numeratorTensDigits[i], 'hidden', true );
-		goog.dom.classes.enable( this._numeratorHundredsDigits[i], 'hidden', true );
+	for ( i = 0; i < l; i++ ) {
+		goog.dom.classes.enable( this._numeratorOnesDigits[ i ], 'hidden', true );
+		goog.dom.classes.enable( this._numeratorTensDigits[ i ], 'hidden', true );
+		goog.dom.classes.enable( this._numeratorHundredsDigits[ i ], 'hidden', true );
 	}
 
 	goog.dom.classes.enable( this._numeratorHundredsEl, 'hidden', true );
 	goog.dom.classes.enable( this._numeratorTensEl, 'hidden', true );
 	goog.dom.classes.enable( this._numeratorOnesEl, 'hidden', true );
 
-	TweenMax.set( [this._innerEl, this._slashEl, this._numeratorEl, this._denominatorEl, this._hintEl], {
+	TweenMax.set( [ this._innerEl, this._slashEl, this._numeratorEl, this._denominatorEl, this._hintEl ], {
 		opacity: 0
-	});
+	} );
 
 	TweenMax.set( this._curtainEl, {
 		display: 'none'
-	});
+	} );
 
 	// preload background image to start intro animation
-	var backgroundSrc = goog.style.getComputedStyle( this._innerEl, 'background-image' ).replace('url(','').replace(')','');
-	var hasBackgroundSrc = (backgroundSrc.length > 0);
+	var backgroundSrc = goog.style.getComputedStyle( this._innerEl, 'background-image' );
+	backgroundSrc = backgroundSrc.match( /url\(["|']?([^"']*)["|']?\)/ )[ 1 ];
 
-	if(hasBackgroundSrc) {
-		
+	var hasBackgroundSrc = ( backgroundSrc.length > 0 );
+
+	if ( hasBackgroundSrc ) {
+
 		var img = new Image();
 		img.src = backgroundSrc;
 
-		goog.events.listenOnce(img, goog.events.EventType.LOAD, this.animateIn, false, this);
+		goog.events.listenOnce( img, goog.events.EventType.LOAD, this.animateIn, false, this );
 
-	}else {
+	} else {
 
 		this.animateIn();
 	}
 };
 
 
-hlc.views.Preloader.prototype.animateIn = function(){
+hlc.views.Preloader.prototype.animateIn = function() {
 
-	goog.base(this, 'animateIn');
+	goog.base( this, 'animateIn' );
 
-	goog.dom.classlist.enable(document.body, 'hidden', false);
+	goog.dom.classlist.enable( document.body, 'hidden', false );
 };
 
 
-hlc.views.Preloader.prototype.animateInCounter = function(){
+hlc.views.Preloader.prototype.animateInCounter = function() {
 
-	TweenMax.fromTo(this._slashEl, .45, {
+	TweenMax.fromTo( this._slashEl, .45, {
 		opacity: 0,
 		x: 30,
 		y: -40
@@ -158,66 +160,66 @@ hlc.views.Preloader.prototype.animateInCounter = function(){
 		x: 0,
 		y: 0,
 		ease: Expo.easeInOut
-	});
+	} );
 
-	TweenMax.fromTo(this._numeratorEl, .45, {
+	TweenMax.fromTo( this._numeratorEl, .45, {
 		opacity: 0,
 		x: -6,
 		y: 10
-	},{
+	}, {
 		delay: .05,
 		opacity: 1,
 		x: 0,
 		y: 0
-	});
+	} );
 
-	TweenMax.fromTo(this._denominatorEl, .45, {
+	TweenMax.fromTo( this._denominatorEl, .45, {
 		opacity: 0,
 		x: -6,
 		y: 10
-	},{
+	}, {
 		delay: .10,
 		opacity: 1,
 		x: 0,
 		y: 0
-	});
+	} );
 
-	var denominatorDigits = goog.dom.query('.digits div', this._denominatorEl);
-	goog.array.forEach(denominatorDigits, function(el, index) {
-		TweenMax.fromTo(el, .45, {
+	var denominatorDigits = goog.dom.query( '.digits div', this._denominatorEl );
+	goog.array.forEach( denominatorDigits, function( el, index ) {
+		TweenMax.fromTo( el, .45, {
 			opacity: 0,
 			x: -3,
 			y: 7
-		},{
+		}, {
 			delay: index * 0.05 + .30,
 			opacity: 1,
 			x: 0,
 			y: 0
-		});
-	});
+		} );
+	} );
 
-	TweenMax.fromTo(this._hintEl, .65, {
+	TweenMax.fromTo( this._hintEl, .65, {
 		opacity: 0,
 		y: 10
 	}, {
 		delay: .65,
 		opacity: 1,
 		y: 0
-	});
+	} );
 };
 
 
 hlc.views.Preloader.prototype.renderProgress = function() {
 
-	var progress = Math.round(this._animationProgress * 100);
+	var progress = Math.round( this._animationProgress * 100 );
 
-	var hasProgress = (progress >= 1);
+	var hasProgress = ( progress >= 1 );
 
-	if(!hasProgress) {
+	if ( !hasProgress ) {
 
 		return;
 
-	}else if(!this._hasProgress) {
+	} else if ( !this._hasProgress ) {
 
 		this._hasProgress = true;
 
@@ -228,63 +230,63 @@ hlc.views.Preloader.prototype.renderProgress = function() {
 
 	var ones, tens, hundreds;
 
-	if(progressStr.length === 3) {
+	if ( progressStr.length === 3 ) {
 
-		ones = parseInt(progressStr[2]);
-		tens = parseInt(progressStr[1]);
-		hundreds = parseInt(progressStr[0]);
+		ones = parseInt( progressStr[ 2 ] );
+		tens = parseInt( progressStr[ 1 ] );
+		hundreds = parseInt( progressStr[ 0 ] );
 
-	}else if(progressStr.length === 2) {
-		
-		ones = parseInt(progressStr[1]);
-		tens = parseInt(progressStr[0]);
+	} else if ( progressStr.length === 2 ) {
 
-	}else if(progressStr.length === 1) {
-		
-		ones = parseInt(progressStr[0]);
+		ones = parseInt( progressStr[ 1 ] );
+		tens = parseInt( progressStr[ 0 ] );
+
+	} else if ( progressStr.length === 1 ) {
+
+		ones = parseInt( progressStr[ 0 ] );
 	}
 
 	// update ones
-	if(goog.isNumber(ones)) {
+	if ( goog.isNumber( ones ) ) {
 
-		goog.array.forEach(this._numeratorOnesDigits, function(digit, index) {
+		goog.array.forEach( this._numeratorOnesDigits, function( digit, index ) {
 
-			var shouldShow = (index === ones);
+			var shouldShow = ( index === ones );
 			goog.dom.classes.enable( digit, 'hidden', !shouldShow );
-		});
+		} );
 
 		goog.dom.classes.enable( this._numeratorOnesEl, 'hidden', false );
 	}
 
 	// update tens
-	if(goog.isNumber(tens)) {
+	if ( goog.isNumber( tens ) ) {
 
-		goog.array.forEach(this._numeratorTensDigits, function(digit, index) {
+		goog.array.forEach( this._numeratorTensDigits, function( digit, index ) {
 
-			var shouldShow = (index === tens);
+			var shouldShow = ( index === tens );
 			goog.dom.classes.enable( digit, 'hidden', !shouldShow );
-		});
+		} );
 
 		goog.dom.classes.enable( this._numeratorTensEl, 'hidden', false );
 	}
 
 	// update hundreds
-	if(goog.isNumber(hundreds)) {
+	if ( goog.isNumber( hundreds ) ) {
 
-		goog.array.forEach(this._numeratorHundredsDigits, function(digit, index) {
+		goog.array.forEach( this._numeratorHundredsDigits, function( digit, index ) {
 
-			var shouldShow = (index === hundreds);
+			var shouldShow = ( index === hundreds );
 			goog.dom.classes.enable( digit, 'hidden', !shouldShow );
-		});
+		} );
 
 		goog.dom.classes.enable( this._numeratorHundredsEl, 'hidden', false );
 	}
 };
 
 
-hlc.views.Preloader.prototype.onProgress = function(e) {
+hlc.views.Preloader.prototype.onProgress = function( e ) {
 
-	goog.base(this, 'onProgress', e);
+	goog.base( this, 'onProgress', e );
 
 	this._animationProgress = e.progress;
 
@@ -292,28 +294,28 @@ hlc.views.Preloader.prototype.onProgress = function(e) {
 };
 
 
-hlc.views.Preloader.prototype.onImageLoad = function(e) {
+hlc.views.Preloader.prototype.onImageLoad = function( e ) {
 
-	goog.base(this, 'onImageLoad', e);
+	goog.base( this, 'onImageLoad', e );
 
-	this.assets[e.target.id] = e.target;
+	this.assets[ e.target.id ] = e.target;
 };
 
 
-hlc.views.Preloader.prototype.onBulkSuccess = function(e) {
+hlc.views.Preloader.prototype.onBulkSuccess = function( e ) {
 
 	var responseText = e.target.getResponseTexts();
-	this.assets['sitemap'] = JSON.parse(responseText);
+	this.assets[ 'sitemap' ] = JSON.parse( responseText );
 
-	goog.base(this, 'onBulkSuccess', e);
+	goog.base( this, 'onBulkSuccess', e );
 };
 
 
-hlc.views.Preloader.prototype.onAnimatedOut = function(e) {
+hlc.views.Preloader.prototype.onAnimatedOut = function( e ) {
 
-	goog.base(this, 'onAnimatedOut', e);
+	goog.base( this, 'onAnimatedOut', e );
 
-	TweenMax.set(this._domElement, {
+	TweenMax.set( this._domElement, {
 		'display': 'none'
-	});
+	} );
 };
